14c5780eb68aa4dcfb2d6bb0732a86501cd25d9d
Katy Gibson <klgibson528@gmail.com>
2018-04-17 15:38:24 -0500

    working on exercises

--- js-exercises.js
+++ js-exercises.js
@@ -35,11 +35,66 @@
 //   }
 // }
 
-//Print a Square
-function printSquare(x) {
-  var count = 0;
-  while (count < x) {
-    console.log("*" * x);
-    count += 1;
-  }
-}
+// //Print a Square
+// function printSquare(x) {
+//   var count = 0;
+//   while (count < x) {
+//     count += 1;
+//     let str = '*';
+//     console.log(str.repeat(x));
+//   }
+// }
+
+// //Print a Box
+// function printBox(x, y) {
+//   var count = 0;
+//   var str = '*';
+//   var blk = ' ';
+//   console.log(str.repeat(y));
+//   while (count < x - 2) {
+//     count += 1
+//     console.log("*" + blk.repeat(y - 2) + "*")
+//   }
+//   console.log(str.repeat(y));
+// }
+
+// //Banner
+// function banner(message) {
+//   console.log('*'.repeat(message.length + 4));
+//   console.log('* ' + message + ' *');
+//   console.log('*'.repeat(message.length + 4));
+// }
+
+// //Leetspeak
+// function leetspeak(message) {
+//   let arr = [];
+//   message = message.toLowerCase();
+//   for (let i = 0; i < message.length; i++) {
+//     switch (message[i]) {
+//       case 'a':
+//         arr.push('4');
+//         break;
+//       case 'e':
+//         arr.push('3');
+//         break;
+//       case 'g':
+//         arr.push('6');
+//         break;
+//       case 'l':
+//         arr.push('1');
+//         break;
+//       case 'o':
+//         arr.push('0');
+//         break;
+//       case 's':
+//         arr.push('5');
+//         break;
+//       case 't':
+//         arr.push('7');
+//         break;
+//       default:
+//         arr.push(message[i])
+//     }
+//   }
+//   console.log(arr.join(''));
+// }
